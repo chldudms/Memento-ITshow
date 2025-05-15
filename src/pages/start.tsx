22a0177465c3c8d7744b6c0bf342063f5721886a
chldudms <yueun0337@gmail.com>
2025-05-15 19:47:18 +0900

    웹 시작 페이지 구현

--- src/pages/start.tsx
+++ src/pages/start.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import MainLayout from '../layouts/mainLayout';
-
-const Start: React.FC = () => {
+import '../styles/start.css'
+import { useNavigate } from 'react-router-dom';
+const Start= () => {
+    const navigate = useNavigate();
+    
     return (
-        <div>
-            <h2>memento</h2>
-            <p>create your diary</p>
-            <button>start</button>
-
+        <div className="container">
+            <img src="/img/diarypage.png" alt="diary" className="diaryImg" />
+            <div className="overlay">
+                <h1 className="logo">MEMENTO</h1>
+                <button className="startBtn" onClick={()=>navigate('/home')}>시작 하기</button>
+            </div>
         </div>
-           
+
     );
 };
 
